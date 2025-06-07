038299ec6a1a9eaa6e61a081f70235f92ef74cf6
shevtsovsv <shevtsovsv@yahoo.com>
2025-06-07 09:02:09 +0300

    8.1.7 Добавит блокировку на момент загрузки данных

--- src/main.js
+++ src/main.js
@@ -41,3 +41,6 @@ const filterPresenter = new FilterPresenter({
 filterPresenter.init();
 boardPresenter.init();
 pointModel.init();
+tasksModel.init().finally(() => {
+  render(newTaskButtonComponent, siteHeaderElement);
+});
